333444e34bac92c029496f7ab634bdd866ba93c4
Bruno Dias <brunodias40@gmail.com>
2024-08-14 14:10:36 -0300

    feat: add component Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <div></div>
+      <a href="https:/github.com/eobrunodias">by | <span>eobrunodias 💻 </span></a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: var(--secondary-color);
+  height: 6rem;
+  display: flex;
+}
+
+a {
+  span {
+    color: #d1cccc;
+  }
+}
+
+.wrapper {
+  grid-area: header;
+
+  width: 100%;
+  margin: 0 auto;
+  max-width: 80rem;
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 0 1rem;
+}
+</style>
