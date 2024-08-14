ef77ffdc88678aadd3fc3ad6aa771d8022e6e204
Bruno Dias <brunodias40@gmail.com>
2024-08-14 16:47:24 -0300

    style: add dinamic style in  menu header

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,42 @@ import { useSearchHistory } from "./stores/useSearchHistory"
 const searchHistory = useSearchHistory()
 
 searchHistory.loadFromLocalStorage()
+
+import { useRoute } from "vue-router"
+
+const route = useRoute()
+
+const isActive = (path: string) => {
+  return route.path === path
+}
 </script>
 
 <template>
   <div class="app-container">
     <Header>
-      <li>
+      <li :class="{ active: isActive('/') }">
         <RouterLink to="/">Home</RouterLink>
       </li>
-      <li>
+      <li :class="{ active: isActive('/history') }">
         <RouterLink to="/history">History</RouterLink>
       </li>
     </Header>
 
-    <RouterView style="min-height: 100vh" />
+    <RouterView />
 
     <Footer />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav a:hover:not(.active) {
+  padding: 0;
+  border-radius: 10px 10px 0 0;
+  border-bottom: 4px solid var(--accent);
+  transition: 0.2ms;
+}
+
+nav .active a {
+  border-bottom: 4px solid var(--accent);
+}
+</style>
